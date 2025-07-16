dd89fb387e16d78467ae7b0d5331f85edc943a92
Lucastro99 <lucas_rcastro1@hotmail.com>
2025-07-16 18:38:57 -0300

    Update script URL to use local proxy for Unlayer editor in multiple files

--- src/components/loadScript.js
+++ src/components/loadScript.js
@@ -1,4 +1,4 @@
-const scriptUrl = 'https://gist.githubusercontent.com/Lucastro99/420705a44c6ce488e1629ee008d4fc06/raw/4b52844743cd0f30d5ef1b2238e864f7310c2062/embed.js';
+const scriptUrl = 'http://192.168.1.156:8989/proxy-unlayer?url=https://gist.githubusercontent.com/Lucastro99/420705a44c6ce488e1629ee008d4fc06/raw/4b52844743cd0f30d5ef1b2238e864f7310c2062/embed.js';
 const callbacks = [];
 let loaded = false;
 
