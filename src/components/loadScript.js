9e06f7b5e03cf48a6d0896f456480bcad5278771
Lucastro99 <lucas_rcastro1@hotmail.com>
2025-07-16 18:52:17 -0300

    Update script URL to point to the hosted version for Unlayer editor in loadScript and distribution files

--- src/components/loadScript.js
+++ src/components/loadScript.js
@@ -1,4 +1,4 @@
-const scriptUrl = 'http://192.168.1.156:8989/proxy-unlayer?url=https://gist.githubusercontent.com/Lucastro99/420705a44c6ce488e1629ee008d4fc06/raw/4b52844743cd0f30d5ef1b2238e864f7310c2062/embed.js';
+const scriptUrl = 'https://testing-unlayer-editor.s3.us-east-1.amazonaws.com/unlayer-editor.js';
 const callbacks = [];
 let loaded = false;
 
